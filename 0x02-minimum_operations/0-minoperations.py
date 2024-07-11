a61fdb62caa704adb109dea9fa736d985fd3955b
manonaSamir <bena.yalla@yahoo.com>
2024-07-11 16:15:40 +0300

    min operation

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''Minimum Operations python3 challenge'''
+
+def minOperations(n):
+    '''calculates the fewest number of
+    operations needed to result in exactly n H '''
+    if n <= 0:
+        return 0
+
+    min_ops = float('inf')
+
+    for i in range(1, int(n**0.5) + 1):
+        if n % i == 0:
+            min_ops = min(min_ops, i + n // i)
+
+    return min_ops
